7b717e3f867c229e6f466f9815b78a7dea0bc1cd
junseokku <asegh12@gmail.com>
2024-07-08 00:28:28 +0900

    fix: 스크립트 내용 수정

--- packages/create-test-package/bin/generate-app.js
+++ packages/create-test-package/bin/generate-app.js
@@ -7,33 +7,66 @@ const { execSync } = require("child_process");
 const GIT_REPO_URL =
   "https://github.com/junseokku/node_typescript_projects_boilerplate.git";
 
-const TARGET_DIR = "packages/test-package";
+const TARGET_DIR = "/packages/test-package/";
+
+if (process.argv.length < 3) {
+  console.error("Please provide a name for the app");
+  process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+
+if (projectName !== ".") {
+  try {
+    fs.mkdirSync(projectPath);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      console.error(`The file ${projectPath} already exists`);
+    } else {
+      console.error(err);
+    }
+    process.exit(1);
+  }
+}
 
 const main = async () => {
   try {
+    if (projectName !== ".") {
+      process.chdir(projectPath);
+    }
+
     console.log("Downloading files...");
 
     runCommand("git init");
     runCommand(`git remote add origin ${GIT_REPO_URL}`);
 
     // 3. sparse-checkout 활성화 및 원하는 디렉토리 설정
-    runCommand("git sparse-checkout init --cone");
-    runCommand(`git sparse-checkout set ${TARGET_DIR}`);
+    runCommand("git config core.sparseCheckout true");
+    fs.writeFileSync(
+      path.join(projectPath, ".git/info/sparse-checkout"),
+      `${TARGET_DIR}\n`
+    );
 
     // 4. 원격 저장소에서 가져오기
-    // runCommand("git pull origin master");
+    runCommand("git pull origin master");
 
-    runCommand(`git clone --depth 1 ${GIT_REPO_URL} ${projectPath}`);
+    // 5. TARGET_DIR 내용을 root로 옮기기
+    runCommand(`mv ${projectPath}${TARGET_DIR}* ${projectPath}`);
 
-    if (projectName !== ".") {
-      process.chdir(projectPath);
-    }
+    // 6. TARGET_DIR 삭제
+    runCommand(`rm -rf ${projectPath}/packages`);
 
     console.log("Installing dependencies...");
-    execSync("npm install");
+    execSync("npm install --legacy-peer-deps");
 
     console.log("Removing .git folder...");
     execSync("rm -rf .git");
+
+    // 7. git 다시 설정 -> .gitignore 추가 등등..
+    runCommand("git init");
+    fs.writeFileSync(path.join(projectPath, ".gitignore"), "/node_modules");
 
     console.log("Project is ready");
   } catch (err) {
@@ -51,28 +84,4 @@ const runCommand = (command, options = {}) => {
   }
 };
 
-(async () => {
-  if (process.argv.length < 3) {
-    console.error("Please provide a name for the app");
-    process.exit(1);
-  }
-
-  const projectName = process.argv[2];
-  const currentPath = process.cwd();
-  const projectPath = path.join(currentPath, projectName);
-
-  if (projectName !== ".") {
-    try {
-      await fs.mkdirSync(projectPath);
-    } catch (err) {
-      if (err.code === "EEXIST") {
-        console.error(`The file ${projectPath} already exists`);
-      } else {
-        console.error(err);
-      }
-      process.exit(1);
-    }
-  }
-
-  void main();
-})();
+main();
