a3cd21ea149ad22521bf376741b8b0d19dafcc3c
junseokku <asegh12@gmail.com>
2024-07-07 23:10:15 +0900

    fix: test-package만 가져오도록 변경

--- packages/create-test-package/bin/generate-app.js
+++ packages/create-test-package/bin/generate-app.js
@@ -4,35 +4,26 @@ const fs = require("fs");
 const path = require("path");
 const { execSync } = require("child_process");
 
-if (process.argv.length < 3) {
-  console.error("Please provide a name for the app");
-  process.exit(1);
-}
-
-const projectName = process.argv[2];
-const currentPath = process.cwd();
-const projectPath = path.join(currentPath, projectName);
-
 const GIT_REPO_URL =
   "https://github.com/junseokku/node_typescript_projects_boilerplate.git";
 
-if (projectName !== ".") {
-  try {
-    await fs.mkdirSync(projectPath);
-  } catch (err) {
-    if (err.code === "EEXIST") {
-      console.error(`The file ${projectPath} already exists`);
-    } else {
-      console.error(err);
-    }
-    process.exit(1);
-  }
-}
+const TARGET_DIR = "packages/test-package";
 
 const main = async () => {
   try {
     console.log("Downloading files...");
-    execSync(`git clone --depth 1 ${GIT_REPO_URL} ${projectPath}`);
+
+    runCommand("git init");
+    runCommand(`git remote add origin ${GIT_REPO_URL}`);
+
+    // 3. sparse-checkout 활성화 및 원하는 디렉토리 설정
+    runCommand("git sparse-checkout init --cone");
+    runCommand(`git sparse-checkout set ${TARGET_DIR}`);
+
+    // 4. 원격 저장소에서 가져오기
+    // runCommand("git pull origin master");
+
+    runCommand(`git clone --depth 1 ${GIT_REPO_URL} ${projectPath}`);
 
     if (projectName !== ".") {
       process.chdir(projectPath);
@@ -49,3 +40,39 @@ const main = async () => {
     console.error(err);
   }
 };
+
+const runCommand = (command, options = {}) => {
+  try {
+    execSync(command, { stdio: "inherit", ...options });
+  } catch (err) {
+    console.error(`Error executing command: ${command}`);
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+(async () => {
+  if (process.argv.length < 3) {
+    console.error("Please provide a name for the app");
+    process.exit(1);
+  }
+
+  const projectName = process.argv[2];
+  const currentPath = process.cwd();
+  const projectPath = path.join(currentPath, projectName);
+
+  if (projectName !== ".") {
+    try {
+      await fs.mkdirSync(projectPath);
+    } catch (err) {
+      if (err.code === "EEXIST") {
+        console.error(`The file ${projectPath} already exists`);
+      } else {
+        console.error(err);
+      }
+      process.exit(1);
+    }
+  }
+
+  void main();
+})();
